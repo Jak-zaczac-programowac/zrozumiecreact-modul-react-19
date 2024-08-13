bb0b0bf82f6e512fdcbf91408112fe5f7a8d1a97
Kacper Sokołowski <kontakt@kaapa.pl>
2024-08-13 20:58:34 +0200

    Use action state koniec

--- use-form-status/front-end/src/components/NewForm/NewForm.jsx
+++ use-form-status/front-end/src/components/NewForm/NewForm.jsx
@@ -1,40 +1,69 @@
-import { useState } from "react";
+import { useActionState } from "react";
 import styles from "./NewForm.module.css";
-import { useFormStatus } from "react-dom";
+import { SubmitButton } from "./SubmitButton";
 
-function SubmitButton() {
-    const { pending } = useFormStatus();
-
-    return (
-        <button className={styles.submitButton}>
-            {pending ? "Zapisuję..." : "Zapisz"}
-        </button>
-    );
+function createComment(formData) {
+    return fetch("http://localhost:3000/comments", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: formData.get("name"),
+            comment: formData.get("comment"),
+        }),
+    });
 }
 
 export function NewForm() {
-    function handleSubmit(formData) {
-        return fetch("http://localhost:3000/comments", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                name: formData.get("name"),
-                comment: formData.get("comment"),
-            }),
-        });
+    function handleSubmit(previousState, formData) {
+        const state = {
+            name: formData.get("name"),
+            comment: formData.get("comment"),
+        };
+
+        if (formData.get("name").length === 0) {
+            state.nameError = true;
+        }
+
+        if (formData.get("comment").length === 0) {
+            state.commentError = true;
+        }
+
+        if (state.nameError || state.commentError) {
+            return state;
+        } else {
+            return createComment(formData).catch(() => {
+                state.formError = true;
+                return state;
+            });
+        }
     }
 
+    const [state, formAction] = useActionState(handleSubmit, {});
+
     return (
-        <form action={handleSubmit}>
+        <form action={formAction}>
+            {state.formError && <p className={styles.error}>Błąd zapisu</p>}
             <label>
                 <p>Imię:</p>
-                <input type="text" name="name" />
+                <input type="text" name="name" defaultValue={state.name} />
+                {state.nameError && (
+                    <p className={styles.error}>Imię nie może być puste!</p>
+                )}
             </label>
             <label>
                 <p>Treść komentarza:</p>
-                <textarea className={styles.textarea} name="comment" />
+                <textarea
+                    className={styles.textarea}
+                    name="comment"
+                    defaultValue={state.comment}
+                />
+                {state.commentError && (
+                    <p className={styles.error}>
+                        Komentarz nie może być pusty!
+                    </p>
+                )}
             </label>
 
             <SubmitButton />
--- use-form-status/front-end/src/components/NewForm/SubmitButton.jsx
+++ use-form-status/front-end/src/components/NewForm/SubmitButton.jsx
@@ -1,5 +1,6 @@
 import styles from "./NewForm.module.css";
 import { useFormStatus } from "react-dom";
+
 export function SubmitButton() {
     const { pending } = useFormStatus();
 
